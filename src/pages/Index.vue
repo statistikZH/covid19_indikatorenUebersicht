3cf803c96bb643b7ecdb8517a04d4f92ed703732
Michel Binder <gitea@fake.local>
2020-04-01 08:38:19 +0200

    fix: only first two bold

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -69,11 +69,11 @@
                 </div>
                 <div class="col-12 col-sm-6 col-md-4">
                   <q-item-label caption>Von - bis</q-item-label>
-                  <q-item-label class="text-weight-bold">{{ props.row.range }}</q-item-label>
+                  <q-item-label>{{ props.row.range }}</q-item-label>
                 </div>
                 <div class="col-12 col-sm-6 col-md-2">
                   <q-item-label caption>Periodizität</q-item-label>
-                  <q-item-label class="text-weight-bold">{{ props.row.update }}</q-item-label>
+                  <q-item-label>{{ props.row.update }}</q-item-label>
                 </div>
                 <div class="col-12 col-sm-6 col-md-2">
                   <q-item-label caption>Abdeckung</q-item-label>
